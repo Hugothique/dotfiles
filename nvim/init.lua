fd24337193cc121209f1aac2ab0d241c6986d69b
Hugo C. <hugo.chalot@telecomnancy.net>
2026-01-23 19:40:44 +0100

    lsp

--- nvim/init.lua
+++ nvim/init.lua
@@ -6,3 +6,51 @@ require("config.lazy")
 require("config.mappings")
 
 require("bufferline").setup{}
+
+vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+  callback = function()
+
+    -- try_lint without arguments runs the linters defined in `linters_by_ft`
+    -- for the current filetype
+    require("lint").try_lint()
+
+    -- You can call `try_lint` with a linter name or a list of names to always
+    -- run specific linters, independent of the `linters_by_ft` configuration
+    -- require("lint").try_lint("cspell")
+  end,
+})
+
+-- vim.diagnostic.config({ virtual_text = true })
+vim.diagnostic.config({ virtual_lines = true })
+
+-- C/C++ {{{
+vim.lsp.config.clangd = {
+  cmd = {
+    "clangd",
+    "-j=" .. 2,
+    "--background-index",
+    "--clang-tidy",
+    "--inlay-hints",
+    "--fallback-style=llvm",
+    "--all-scopes-completion",
+    "--completion-style=detailed",
+    "--header-insertion=iwyu",
+    "--header-insertion-decorators",
+    "--pch-storage=memory",
+  },
+  filetypes = { "c", "cpp", "objc", "objcpp", "cuda", "proto" },
+  root_markers = {
+    "CMakeLists.txt",
+    ".clangd",
+    ".clang-tidy",
+    ".clang-format",
+    "compile_commands.json",
+    "compile_flags.txt",
+    "configure.ac",
+    ".git",
+    vim.uv.cwd(),
+  },
+}
+vim.lsp.enable("clangd")
+-- }}}
+
---
+++ nvim/lua/plugins/nvim-lint.lua
@@ -0,0 +1,14 @@
+return {
+	'mfussenegger/nvim-lint',
+	config = function()
+		require('lint').linters_by_ft = { --some of these need to be installed from package manager
+		  lua = {'luac'},
+		  python = {'ruff'},
+		  sh = {'bash'},
+		  c = {'clangtidy'},
+		  rust = {'clippy'},
+		  css = {'stylelint'},
+		  html = {'htmlhint'},
+		}
+	end,
+}
